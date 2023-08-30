625de0c3326ebdaa33c4c22758e2f89f349b617e
Juan Esteban Cardona Ospina <juan.cardona@ibox.com.co>
2023-08-29 22:14:35 -0500

    taller 2

--- movie/views.py
+++ movie/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Movie
+
 def home(request):
-    return render(request, 'home.html', {'name':'Juan Cardona'})
+    #  return render(request, 'home.html', {'name':'Juan Cardona'})
+     searchTerm = request.GET.get('searchMovie')
+     if searchTerm: 
+        movies = Movie.objects.filter(title_icontains=searchTerm)
+     else:   
+        movies = Movie.objects.all()
+     return render(request, 'home.html', {'movies' : movies})
+
+def about(request):
+     return render(request, 'about.html')
 
 
 # Create your views here.
--- moviereviews/urls.py
+++ moviereviews/urls.py
@@ -14,11 +14,25 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import os
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from movie import views as movieViews
+from moviereviews.settings import BASE_DIR
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', movieViews.home),
+    path('', movieViews.home, name='home'),
+    path('about/', movieViews.about),
+    path('news/', include('news.urls')),
 ]
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    'moviereviews/static/',
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
